30f693c4447c0259e40d0ef0da4bdd7ec3f82a9e
Fredrik Wallgren <fredrik.wallgren@gmail.com>
2010-10-28 23:45:10 +0200

    Add demo extension to fabric-ui

--- application.js
+++ application.js
@@ -1,4 +1,18 @@
 $(function(){
-  $('#fabric-ui').fabricUi();
+  var fabricUiDiv = $('#fabric-ui');
+  var fabricUi = fabricUiDiv.fabricUi().data('fabricUi');
+
+  var button = $('<button>Alert JSON</button>');
+  button.click(function() {
+    alert(fabricUi.toJSON());
+  });
+  fabricUiDiv.append(button);
+
+  var button = $('<button>LOAD JSON</button>');
+  button.click(function() {
+    var data = prompt('Insert jsqon', '');
+    fabricUi.loadJSON(data);
+  });
+  fabricUiDiv.append(button);
 });
 
